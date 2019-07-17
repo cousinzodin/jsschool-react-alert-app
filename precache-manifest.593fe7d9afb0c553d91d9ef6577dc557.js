6dc3016c76743edd84d01dd83a35c23eb3dbaca3
Alexandra Gor <alexandra.goryacheva@streamtechltd.com>
2019-07-17 12:07:05 +0300

    Updates

---
+++ precache-manifest.593fe7d9afb0c553d91d9ef6577dc557.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6b0afd7cb61c01de30cc3a647ae9f59",
+    "url": "/jsschool-react-alert-app/index.html"
+  },
+  {
+    "revision": "4036c49d4ed4aa70f25e",
+    "url": "/jsschool-react-alert-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2ca676763a994825257c",
+    "url": "/jsschool-react-alert-app/static/js/2.6d4116ab.chunk.js"
+  },
+  {
+    "revision": "4036c49d4ed4aa70f25e",
+    "url": "/jsschool-react-alert-app/static/js/main.d87f058e.chunk.js"
+  },
+  {
+    "revision": "6f57f20c46ea6c9dde4b",
+    "url": "/jsschool-react-alert-app/static/js/runtime~main.faaa0b47.js"
+  }
+]);
